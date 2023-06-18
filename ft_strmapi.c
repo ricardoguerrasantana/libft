3bf5119c9d9cdfa7909c1f82253674e3de26e146
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2023-06-18 18:19:39 +0930

    Add ft_strmapi function

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricguerr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 19:06:32 by ricguerr          #+#    #+#             */
+/*   Updated: 2023/05/18 19:24:38 by ricguerr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		i;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	str = (char *)malloc((ft_strlen(s) +1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
