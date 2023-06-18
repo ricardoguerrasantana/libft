23bad7140938cddbd1bb4e17110fcbcdb13d8c78
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2023-06-18 18:19:38 +0930

    Add ft_strnstr function

---
+++ ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricguerr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 20:07:19 by ricguerr          #+#    #+#             */
+/*   Updated: 2023/05/17 21:57:08 by ricguerr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	nlen;
+
+	nlen = ft_strlen(needle);
+	if (!nlen)
+		return ((char *)haystack);
+	while (*haystack && len >= nlen)
+	{
+		if (ft_strncmp(haystack, needle, nlen) == 0)
+			return ((char *)haystack);
+		haystack++;
+		len--;
+	}
+	return (0);
+}
